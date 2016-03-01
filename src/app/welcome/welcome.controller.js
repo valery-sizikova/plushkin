df8907e28b0dfea6f6d896ed856d5f8c8d8500a6
Valery Sizikova <valery.sizikova@gmail.com>
2016-03-02 00:18:42 +0100

    updated and styled welcome page

--- src/app/welcome/welcome.controller.js
+++ src/app/welcome/welcome.controller.js
@@ -4,7 +4,6 @@
   angular.module('app.welcome')
     .controller('WelcomeController', ['$scope', '$location', 'MainService', function($scope, $location, MainService) {
       var vm = this;
-      vm.wow = "it works!";
       vm.go = MainService.go;
 
     }]);
